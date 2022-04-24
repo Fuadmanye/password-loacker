5dd112089b5b5dab2ec6addc25665c500cb4e409
fuad manye / <fahadbasi333@gmail.com>
2022-04-24 14:43:03 +0300

    create test 1

--- user_test.py
+++ user_test.py
@@ -9,3 +9,24 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+     # Items up here .......
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Fuad","12345") # create user object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.user_name,"Fuad")
+        self.assertEqual(self.new_user.password,"12345")
+        
+
+if __name__ == '__main__':
+    unittest.main()
