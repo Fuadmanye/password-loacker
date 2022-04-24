6865f76dedf0ca76e5aadfa54f74ad0808bb4ccc
fuad manye / <fahadbasi333@gmail.com>
2022-04-24 15:24:54 +0300

    add  @classmethod

--- user.py
+++ user.py
@@ -25,3 +25,10 @@ class User:
         '''
         User.user_list.remove(self)
         
+    @classmethod
+    def display_users(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list    
+        
